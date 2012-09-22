8b5d218c293a76eec49f366bdeb94219cfefee76
Lewis McLean <mclean.lewis@gmail.com>
2012-09-22 09:01:09 +0100

    added some ssl examples

--- Ticker/src/stock_server/RunMain.java
+++ Ticker/src/stock_server/RunMain.java
@@ -1,45 +1,18 @@
 package stock_server;
+
 import java.io.IOException;
 
-
-
-
-
-
 public class RunMain {
-	
-	
 
-		public static void main(String[] args) throws IOException {
-			
-			
-			//DBconnect oracle = new DBconnect();
-			
-			//SocketServer socketConn = new SocketServer(); 
-			
-			
-			
-			getData test = new getData();
-			
-			
-			for (int i = 3; i>2;i++){
-				test.getResults();
-			
-			}
-			
-			}
-			
-			
-		
-			
-		
+	public static void main(String[] args) throws IOException {
+
+		getData test = new getData();
+
+		for (int i = 3; i > 2; i++) {
+			test.getResults();
+
 		}
-			
-			
-			
-			
-				//}//end if
-	
-	
-	
 
+	}
+
+}
